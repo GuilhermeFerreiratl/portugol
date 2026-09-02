9d172c07d4ac9b5b9a875141e2c039412acad62a
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-09-02 11:24:19 -0300

    Add age classification program in Portuguese

---
+++ criança,jovem,adulto,idoso.cpp
@@ -0,0 +1,23 @@
+programa {
+  funcao inicio() {
+
+inteiro idade
+
+escreva ("qual sua idade? ")
+leia (idade)
+
+se (idade >=0 e idade <=12){
+escreva ("crianca")
+}
+senao se (idade >12 e idade <18){
+  escreva ("adolescente")
+}
+senao se (idade >=18 e idade <=60){
+  escreva ("adulto")
+}
+senao se (idade >60 ){
+  escreva ("idoso")
+}
+    
+  }
+}
