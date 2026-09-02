df1146a87ae1ae3fda2cf744ef0227d84ee28769
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-09-02 11:23:39 -0300

    Add program to check if it's raining

---
+++ esta chovendo.cpp
@@ -0,0 +1,31 @@
+programa {
+  funcao inicio() {
+
+logico chovendo
+
+ escreva ("esta chovendo?")
+ leia (chovendo)
+
+se (chovendo == verdadeiro){
+  escreva ("use guarda chuva!")
+}
+senao {
+  escreva ("esta fazendo sol")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  }
+}
